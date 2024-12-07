ab8e0743e8e8f5dc595210d08f2b3011763cd3b5
Erhahon Imafidon <mafiosco4@gmail.com>
2024-12-07 14:17:00 +0100

    Styled the card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-
+import { Cards } from './components/index.tsx'
 const App = () => {
   return (
-    <div>
-       <h1>React App</h1>
-    </div>
+    <>
+       <Cards />
+    </>
   )
 }
 
---
+++ src/components/Cards.tsx
@@ -0,0 +1,11 @@
+
+const Cards = () => {
+  return (
+    <div className='cards'>
+       <h1 className='card-title'>Card Title</h1>
+        <p className='card-body'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. <a href='#'>Accusantium</a> alias animi assumenda excepturi laborum maiores quae quasi sequi ullam unde.</p>
+    </div>
+  )
+}
+
+export default Cards
