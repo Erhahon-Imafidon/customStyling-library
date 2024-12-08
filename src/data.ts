546eced723a9bc16f198a01a579b0d4deea6c4a9
Erhahon Imafidon <mafiosco4@gmail.com>
2024-12-09 00:04:39 +0100

    Added hover fnctions and variations to it

--- src/data.ts
+++ src/data.ts
@@ -2,7 +2,7 @@ export const colorsData = [
     { textColor: 'text-primary', text: 'primary text' },
     { textColor: 'text-secondary', text: 'secondary text' },
     { textColor: 'text-error', text: 'error text' },
-    { textColor: 'text-info', text: 'info text' },
+    { textColor: 'text-info text-hover-blue-light-8', text: 'info text' },
     { textColor: 'text-blue', text: 'blue text' },
     { textColor: 'text-red', text: 'red text' },
     { textColor: 'text-green', text: 'green text' },
